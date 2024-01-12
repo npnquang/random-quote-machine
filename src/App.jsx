781c59728c454e1ee1d2132ad3822ebcb722b688
Pham Nhat Quang Nguyen <nguyenjoe0201@gmail.com>
2024-01-12 14:58:01 +0700

    Make the NewQuoteButton interactive

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,26 @@ import { useState } from 'react'
 import Text from "./components/Text.jsx"
 import Author from './components/Author.jsx'
 import NewQuoteButton from './components/NewQuoteButton.jsx'
-
+import quotes from './components/quotes.js'
 
 function App() {
   
+  function getQuote() {
+    return quotes[Math.floor(Math.random() * (quotes.length - 1))];
+  }
+
+  const initialQuoteObject = getQuote();
+
+  const {text, author} = initialQuoteObject;
+
+  const [textDisplay, setText] = useState(text);
+  const [authorDisplay, setAuthor] = useState(author);
   
   return (
     <div id="quote-box">
-      <Text text={"Hello"} />
-      <Author author={"ME"}/>
-      <NewQuoteButton />
+      <Text text={textDisplay} />
+      <Author author={authorDisplay}/>
+      <NewQuoteButton getQuote={getQuote} changeAuthor={setAuthor} changeText={setText}/>
       {/* <TweetQuoteButton /> */}
     </div>
   )
--- src/components/NewQuoteButton.jsx
+++ src/components/NewQuoteButton.jsx
@@ -1,22 +1,23 @@
 import PropTypes from 'prop-types'
 
 export default function NewQuoteButton({ getQuote, changeText, changeAuthor }) {
-
     const handleClick = () => {
         const newQuote = getQuote();
         const { text, author } = newQuote;
         changeText(text);
         changeAuthor(author);
     }
+
     return (
         <div id="new-quote">
-            <button onClick={handleClick}>New Quote !!!</button>
+            <button onClick={handleClick} >New Quote!!!</button>
         </div>
     )
 }
 
 NewQuoteButton.propTypes = {
-    getQuote: PropTypes.function.isRequired,
-    changeText: PropTypes.function.isRequired,
-    changeAuthor: PropTypes.function.isRequired,
+    getQuote: PropTypes.func.isRequired,
+    changeText: PropTypes.func.isRequired,
+    changeAuthor: PropTypes.func.isRequired,
+
 }
